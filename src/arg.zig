d932ed5ec617e16084c60c2339ff2ef359bce301
lepton <tonihirvonen00@gmail.com>
2025-10-19 18:23:44 +0300

    refactoring

--- src/arg.zig
+++ src/arg.zig
@@ -1,63 +1,100 @@
 const std = @import("std");
+const OptType = @import("parse.zig").OptType;
 
-pub const OptType = enum {
-    long,
-    short,
+pub const Cmd = struct {
+    name: []const u8,
+    desc: []const u8,
+    options: ?[]const Option = null,
 };
 
-pub const OptionParse = struct {
-    option_type: OptType,
+pub const Arg = struct {
     name: []const u8,
     value: ?[]const u8 = null,
+    required: bool = true,
 };
 
-pub const ArgParse = union(enum) {
-    option: OptionParse,
-    value: []const u8,
+pub const Option = struct {
+    long_name: []const u8,
+    short_name: ?[]const u8,
+    desc: []const u8,
+    required: bool = false,
+    arg: ?Arg = null,
+
+    pub fn get_format_name(self: *const Option, buffer: []u8) []const u8 {
+        return std.fmt.bufPrint(buffer, "'--{s}'", .{self.long_name}) catch self.long_name;
+    }
+
+    pub fn format_arg_name(self: *const Option, buffer: []u8) ?[]const u8 {
+        if (self.arg) |arg| {
+            return std.fmt.bufPrint(buffer, "<{s}{s}>", .{
+                if (arg.required) "" else "?",
+                arg.name,
+            }) catch arg.name;
+        }
+        return null;
+    }
 };
 
-pub fn parse_arg(arg: []const u8) ?ArgParse {
-    if (arg.len == 0) return null;
-    if (arg[0] == '-') {
-        if (arg.len == 1) return .{ .value = arg };
+pub const ArgsStructure = struct {
+    cmd_required: bool = false,
+    commands: []const Cmd = &[_]Cmd{},
+    options: []const Option = &[_]Option{},
 
-        var name = arg[1..];
-        var option_type: OptType = .short;
-        if (arg[1] == '-') {
-            if (arg.len == 2) return .{ .value = arg };
-            name = arg[2..];
-            option_type = .long;
+    pub fn as_string(self: *const ArgsStructure, allocator: std.mem.Allocator) ![]const u8 {
+        var line_buf: [256]u8 = undefined;
+        var arg_buf: [32]u8 = undefined;
+        var buf = try std.ArrayList(u8).initCapacity(allocator, 1024);
+        errdefer buf.deinit(allocator);
+        try buf.appendSlice(allocator, "Commands:\n\n");
+        for (self.commands) |cmd| {
+            try buf.appendSlice(allocator, try std.fmt.bufPrint(
+                &line_buf,
+                "  {s:<40} {s}\n",
+                .{ cmd.name, cmd.desc },
+            ));
         }
-
-        if (std.mem.indexOfScalar(u8, name, '=')) |i| {
-            const value = if (name.len <= i + 1) null else name[i + 1 ..];
-            return .{
-                .option = .{
-                    .option_type = option_type,
-                    .name = name[0..i],
-                    .value = value,
-                },
+        try buf.appendSlice(allocator, "\nOptions:\n\n");
+        for (self.options) |opt| {
+            const arg_name = opt.format_arg_name(&arg_buf);
+            const line = blk: {
+                if (opt.short_name) |short| {
+                    break :blk try std.fmt.bufPrint(
+                        &line_buf,
+                        "  -{s}, --{s:<11} {s:<22} {s}\n",
+                        .{ short, opt.long_name, arg_name orelse "", opt.desc },
+                    );
+                } else {
+                    break :blk try std.fmt.bufPrint(
+                        &line_buf,
+                        "      --{s:<11} {s:<22} {s}\n",
+                        .{ opt.long_name, arg_name orelse "", opt.desc },
+                    );
+                }
             };
-        } else {
-            return .{
-                .option = .{
-                    .option_type = option_type,
-                    .name = name,
-                },
-            };
+            try buf.appendSlice(allocator, line);
         }
-    } else {
-        return .{ .value = arg };
+        return buf.toOwnedSlice(allocator);
     }
-}
 
-pub fn parse_args(allocator: std.mem.Allocator, args_str: [][:0]u8) ![]ArgParse {
-    var args = try std.ArrayList(ArgParse).initCapacity(allocator, 5);
-    for (args_str) |token| {
-        const arg: ?ArgParse = parse_arg(token);
-        if (arg) |a| {
-            try args.append(allocator, a);
+    pub fn find_cmd(self: *const ArgsStructure, cmd: []const u8) !Cmd {
+        for (self.commands) |c| {
+            if (std.mem.eql(u8, c.name, cmd)) {
+                return c;
+            }
         }
+        return error.InvalidCommand;
     }
-    return args.toOwnedSlice(allocator);
-}
+
+    pub fn find_option(self: *const ArgsStructure, opt: []const u8, opt_type: OptType) !Option {
+        for (self.options) |o| {
+            const name = switch (opt_type) {
+                OptType.short => o.short_name orelse continue,
+                OptType.long => o.long_name,
+            };
+            if (std.mem.eql(u8, name, opt)) {
+                return o;
+            }
+        }
+        return error.InvalidOption;
+    }
+};
---
+++ src/parse.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+
+pub const OptType = enum {
+    long,
+    short,
+};
+
+pub const OptionParse = struct {
+    option_type: OptType,
+    name: []const u8,
+    value: ?[]const u8 = null,
+};
+
+pub const ArgParse = union(enum) {
+    option: OptionParse,
+    value: []const u8,
+};
+
+fn parse_arg(arg: []const u8) ?ArgParse {
+    if (arg.len == 0) return null;
+    if (arg[0] == '-') {
+        if (arg.len == 1) return .{ .value = arg };
+
+        var name = arg[1..];
+        var option_type: OptType = .short;
+        if (arg[1] == '-') {
+            if (arg.len == 2) return .{ .value = arg };
+            name = arg[2..];
+            option_type = .long;
+        }
+
+        if (std.mem.indexOfScalar(u8, name, '=')) |i| {
+            const value = if (name.len <= i + 1) null else name[i + 1 ..];
+            return .{
+                .option = .{
+                    .option_type = option_type,
+                    .name = name[0..i],
+                    .value = value,
+                },
+            };
+        } else {
+            return .{
+                .option = .{
+                    .option_type = option_type,
+                    .name = name,
+                },
+            };
+        }
+    } else {
+        return .{ .value = arg };
+    }
+}
+
+pub fn parse_args(allocator: std.mem.Allocator, args_str: [][:0]u8) ![]ArgParse {
+    var args = try std.ArrayList(ArgParse).initCapacity(allocator, 5);
+    errdefer args.deinit(allocator);
+    for (args_str) |token| {
+        const arg: ?ArgParse = parse_arg(token);
+        if (arg) |a| {
+            try args.append(allocator, a);
+        }
+    }
+    return args.toOwnedSlice(allocator);
+}
