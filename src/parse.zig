58d7e0263932046096d5d75443f4108b11369088
lepton <tonihirvonen00@gmail.com>
2025-10-29 12:44:56 +0200

    feat: force positional args after `--`

--- src/parse.zig
+++ src/parse.zig
@@ -16,49 +16,40 @@ pub const ArgParse = union(enum) {
     value: []const u8,
 };
 
-fn parse_arg(arg: []const u8) ?ArgParse {
-    if (arg.len == 0) return null;
-    if (arg[0] == '-') {
+fn parse_arg(arg: []const u8, force_positional: *bool) ?ArgParse {
+    if (arg.len > 0 and !force_positional.* and arg[0] == '-') {
         if (arg.len == 1) return .{ .value = arg };
 
         var name = arg[1..];
         var option_type: OptType = .short;
         if (arg[1] == '-') {
-            if (arg.len == 2) return .{ .value = arg };
+            if (arg.len == 2) {
+                force_positional.* = true;
+                return null;
+            }
             name = arg[2..];
             option_type = .long;
         }
-
         if (std.mem.indexOfScalar(u8, name, '=')) |i| {
             const value = if (name.len <= i + 1) null else name[i + 1 ..];
-            return .{
-                .option = .{
-                    .option_type = option_type,
-                    .name = name[0..i],
-                    .value = value,
-                },
-            };
+            return .{ .option = .{
+                .option_type = option_type,
+                .name = name[0..i],
+                .value = value,
+            } };
         } else {
-            return .{
-                .option = .{
-                    .option_type = option_type,
-                    .name = name,
-                },
-            };
+            return .{ .option = .{ .option_type = option_type, .name = name } };
         }
-    } else {
-        return .{ .value = arg };
-    }
+    } else return .{ .value = arg };
 }
 
 pub fn parse_args(allocator: std.mem.Allocator, args_str: [][:0]u8) ![]ArgParse {
-    var args = try std.ArrayList(ArgParse).initCapacity(allocator, 5);
+    var args = try std.ArrayList(ArgParse).initCapacity(allocator, 10);
+    var force_positional = false;
     errdefer args.deinit(allocator);
     for (args_str) |token| {
-        const arg: ?ArgParse = parse_arg(token);
-        if (arg) |a| {
-            try args.append(allocator, a);
-        }
+        const arg: ?ArgParse = parse_arg(token, &force_positional);
+        if (arg) |a| try args.append(allocator, a);
     }
     return args.toOwnedSlice(allocator);
 }
